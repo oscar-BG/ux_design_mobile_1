1fdb23a76b2387dbca92baa8949ea0f95230523f
Oscar Bautista <oscar04262000@gmail.com>
2023-11-28 23:09:54 -0600

    Feature: diseño del login

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:ux_design_mobile_1/widgets/widgets.dart';
+
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthBackground(),
+   );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:ux_design_mobile_1/widgets/auth_background.dart';
