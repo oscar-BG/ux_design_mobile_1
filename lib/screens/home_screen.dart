f84f735845cbf5a93d4c472873bd443f69adca92
Oscar Bautista <oscar04262000@gmail.com>
2023-12-07 10:00:08 -0600

    Feature home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,8 +15,18 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Inicio')),
+        title: const Text('Inicio'),
         backgroundColor: Colors.deepPurple,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.blue[500],
+              child: const Text('OB'),
+            ),
+          )
+        ],
+        centerTitle: true,
       ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
@@ -50,18 +60,34 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: <Widget>[
-        const Card(
-          shadowColor: Colors.transparent,
-          margin:  EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-              ),
+
+        // Pagina de inicio
+         Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children:  <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.album),
+                  title: Text('The Enchanted Nightingale'),
+                  subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('PLAY'),
+                      onPressed: () {/* ... */},
+                    )
+                  ],
+                )
+              ],
             ),
           ),
         ),
 
+        // Pagina de notificaciones
         const Padding(
           padding: EdgeInsets.all(8.0),
           child: Column(
@@ -70,31 +96,63 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: ListTile(
                   leading: Icon(Icons.notifications_sharp),
                   title: Text('Notification 1'),
-                  subtitle: Text('This is a notification'),
+                  subtitle: Text('Esta es una notificación'),
                 ),
               ),
               Card(
                 child: ListTile(
                   leading: Icon(Icons.notifications_sharp),
                   title: Text('Notification 2'),
-                  subtitle: Text('This is a notification'),
+                  subtitle: Text('Esta es una notificación'),
                 ),
               ),
             ],
           ),
         ),
 
-        const Card(
-          shadowColor: Colors.transparent,
-          margin:  EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Configuración',
-              ),
+        // Pagina de configuracion
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            shadowColor: Colors.transparent,
+            child: ListView(
+              children:  <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.person_2, color: Colors.green,),
+                  title: Text('Perfil'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                const Divider(),
+                ListTile(
+                  leading: Icon(Icons.book, color: Colors.amber[400],),
+                  title: const Text('Terminos y Condiciones'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                ),
+                const Divider(),
+                const ListTile(
+                  leading: Icon(Icons.privacy_tip, color: Colors.red,),
+                  title: Text('privacidad'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                const Divider(),
+                const ListTile(
+                  leading: Icon(Icons.help, color: Colors.lightBlueAccent,),
+                  title: Text('Preguntas frecuentes'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                const Divider(),
+                const ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text('Cerrar sesión'),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                ),
+                const Divider(),
+              ],
             ),
-          ),
-        ),
+          )
+        )
+
+
       ][_selectedIndex]
     );
   }
